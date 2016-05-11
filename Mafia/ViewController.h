74853b286480a845129419d317cf971c958decab
Zach Smoroden <zach@smoroden.com>
2016-05-11 14:14:52 -0700

    initial commit

---
+++ Mafia/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Mafia
+//
+//  Created by Zach Smoroden on 2016-05-11.
+//  Copyright © 2016 Zach Smoroden. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
